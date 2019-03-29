cdb537fb9440336523242b7d4e0d34ae72d3ee4c
Siying Li <yukimashiro425@gmail.com>
2019-03-29 18:37:20 -0400

    Mapping And Character Controll

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    // Public Variables
+    public float speed = 10.0f;
+    public float jumpHeight = 10.0f;
+   
+    //public Animator animator;
+
+    // Private Variables
+    private Rigidbody2D rBody;
+    private bool jump = false;
+ 
+     
+    // Start is called before the first frame update
+    // Use this for initialization
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+  
+  
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = 0.00f;
+        if(jump == true)
+        {
+            vert = Input.GetAxis("Jump");
+        }
+
+
+
+        // Debug.Log("H: " + horiz + " , V: " + vert);
+        Vector2 newVelocity = new Vector2(horiz, vert);
+
+        rBody.velocity = newVelocity * speed;
+
+        //animator.SetFloat("direction x", newVelocity.x);
+        //animator.SetFloat("direction y", newVelocity.y);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Ground")
+        {
+            Debug.Log("Reaction");
+            jump = true;
+        }
+    }
+}
